583ac83d8c9ece3c42a8d50207b3d841ce613371
bbrookcss <shgorawebsite@gmail.com>
2024-02-29 17:45:47 +0300

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,46 @@
+<script>
+  import Inpu from "./inputtext.svelte";
+
+  let Visible = false;
+  let accountVisible = true;
+
+  function Visibility() {
+    accountVisible = false;
+
+    setTimeout(() => {
+      Visible = true;
+    }, 2000);
+  }
+</script>
+
+<main>
+  <img src="/img/logo.png" alt="loggo" class="logo">
+  {#if Visible}
+    <button class="topics">◷</button>
+    <button class="upgrade">Upgrade to Pro</button>
+    <button class="logged">logged in as</button>
+    <a href="as" class="feed"><p>Send Feedback 💡</a>
+    <p class="words"><b>14,842 </b>words left</p>
+    <Inpu/>
+  {/if}
+
+  {#if accountVisible}
+    <div class="account">
+      <div class="center">
+        <h2>Continue with</h2>
+        <button on:click={Visibility} class="google">Google</button>
+        <button on:click={Visibility} class="gitHub">GitHub</button>
+        <p>Need help? Contact <a href="as">support@kal.chat</a></p>
+      </div>
+    </div>
+  {/if}
+</main>
+
+<style>
+  .account {
+    display: grid;
+    place-items: center;
+    margin-top: 310px;
+  }
+  
+</style>
---
+++ src/inputtext.svelte
@@ -0,0 +1,55 @@
+<script>
+    let name = '';
+    let nahom = '';
+    let names = [];
+
+    const chnage = () => {
+        if (nahom !== '') {
+            if (name === '') {
+                name = nahom;
+            } else {
+                names = [...names, nahom];
+            }
+            nahom = '';
+        }
+    }
+</script>
+
+<main>
+    <section class="contener">
+        <div class="maincontener">
+            <h6>Choose mode:</h6>
+            <button class="faster">Faster responses <span>GPT 3.5 Turbo</span></button>
+            <button class="better">Better responses <br> ✅ GPT 4  </button>
+            <div>
+                
+            </div>
+            {#if name === ''}
+            <div>
+                <h3>KAL</h3>
+                <h4>Hi Nahom, What can I help you with right now, Nahom?</h4>
+                
+            </div>
+            {:else}
+            <div>
+                <h2>You</h2>
+                <p>{name}</p>
+                
+            </div>
+            {#each names as n}
+            <div>
+                <h2>You</h2>
+                <p>{n}</p>
+            </div>
+            {/each}
+            {/if}
+        </div>
+    </section>
+    <div class="inputfiled">
+        <div class="topic">
+            <h1>+</h1>
+        </div>
+        <input bind:value={nahom} placeholder="Ask me anything" />
+        <button on:click={chnage}>→</button>
+    </div>
+</main>
